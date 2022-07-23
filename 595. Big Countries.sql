85d23317a25fea7ff5a093e6dd84a3ff46335c9e
Nishant Salian <nishant.salian1594@gmail.com>
2022-07-23 17:21:50 -0500

    Update 595. Big Countries.sql

--- 595. Big Countries.sql
+++ 595. Big Countries.sql
@@ -1,3 +1,19 @@
+-- APPROACH 1
 SELECT NAME, POPULATION, AREA
 FROM WORLD
 WHERE AREA >= 3000000 OR POPULATION >= 25000000
+
+-- APPROACH 2 SLOWER
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area >= 3000000
+UNION
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population >= 25000000
